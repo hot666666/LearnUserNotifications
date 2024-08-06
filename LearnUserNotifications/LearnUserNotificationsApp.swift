89eb033c9aa13addac9d0d65788768fd603e0bc3
hot666666 <hotcs6@naver.com>
2024-08-06 16:17:11 +0900

    Initial Commit

---
+++ LearnUserNotifications/LearnUserNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnUserNotificationsApp.swift
+//  LearnUserNotifications
+//
+//  Created by hs on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnUserNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
